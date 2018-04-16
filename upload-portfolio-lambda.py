e85c418e69c2f4668987b4ed17e7287986e50813
Andre Luiz Melo <andreluiz.melo@outlook.com>
2018-04-15 21:59:10 -0300

    publicando sucesso ou insucesso para SNS

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,22 +2,29 @@ import boto3
 import io
 import zipfile
 import mimetypes
+import os
     
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
-    portfolio_bucket = s3.Bucket('portfolio.andreluizmelo.com')
-    build_bucket = s3.Bucket('portfoliobuild.andreluizmelo.com')
+    sns = boto3.resource('sns')
+    topic = sns.Topic(os.environ['publishTopic'])
+    try:
+        portfolio_bucket = s3.Bucket('portfolio.andreluizmelo.com')
+        build_bucket = s3.Bucket('portfoliobuild.andreluizmelo.com')
+        
+        # download to memory the zip file
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        # unzip and upload to the public facing bucket
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
     
-    # download to memory the zip file
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    # unzip and upload to the public facing bucket
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-    print('Job Done!')
+        topic.publish(Subject='Portfolio Deployed', Message='Portfolio deployed successfully!')
+    except:
+        topic.publish(Subject='Portfolio Not Deployed', Message='Portfolio was not deployed successfully!')
+        raise
     return 'Hello from Lambda'
